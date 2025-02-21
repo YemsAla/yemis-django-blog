28dfd54004a1f20641e467a0d5fad5f9e8a79ee9
Yemi Alade <ylawal7@gmail.com>
2025-02-21 16:52:57 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ruha0s!-htv6rv^r^6u5gsjb#s#l&#t4uc__nmbi$_z!39tew'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
