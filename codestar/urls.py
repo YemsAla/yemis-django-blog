b58dee2588bcb43371c619a39035143d77da65c6
Yemi Alade <ylawal7@gmail.com>
2025-06-12 10:59:18 +0100

    Update login.html

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,9 +21,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("about/", include("about.urls"), name="about-urls"),
+    path("about/", include("codestar.about.urls"), name="about-urls"),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include("blog.urls"), name='blog-urls'),
-    
+    path('', include("codestar.blog.urls"), name='blog-urls'),    
 ]
