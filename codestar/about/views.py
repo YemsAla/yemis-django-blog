9c20f309284a12ef33671a8e6ea61286b9c8668a
Yemi Alade <ylawal7@gmail.com>
2025-08-01 13:21:34 +0100

    Handle POST requests

--- codestar/about/views.py
+++ codestar/about/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm 
+from .models import CollaborateRequest
 
 # Create your views here.
 def about_page(request):
@@ -16,6 +18,17 @@ def about_page(request):
 
     :template:`about/about.html`
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
+
+    
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
     
